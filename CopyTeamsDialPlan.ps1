89273cadefcec053f0fb49850d393fca7fa43093
Rob Watts <65971102+robwatts365@users.noreply.github.com>
2025-01-14 12:23:47 +0000

    initial commit

---
+++ CopyTeamsDialPlan.ps1
@@ -0,0 +1,60 @@
+<# 
+Copy Teams Dial Plan Normalisation Rules to another Dial Plan
+    Version: v1.0
+    Date: 14/01/2025
+    Author: Rob Watts - Cloud Solution Architect - Microsoft
+    Description: This script will copy Teams Dial Plan Normalisation Rules to another Dial Plan.
+
+DISCLAIMER
+   THIS CODE IS SAMPLE CODE. THESE SAMPLES ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
+   MICROSOFT FURTHER DISCLAIMS ALL IMPLIED WARRANTIES INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES
+   OF MERCHANTABILITY OR OF FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK ARISING OUT OF THE USE OR
+   PERFORMANCE OF THE SAMPLES REMAINS WITH YOU. IN NO EVENT SHALL MICROSOFT OR ITS SUPPLIERS BE LIABLE FOR
+   ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS
+   INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING OUT OF THE USE OF OR
+   INABILITY TO USE THE SAMPLES, EVEN IF MICROSOFT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+   BECAUSE SOME STATES DO NOT ALLOW THE EXCLUSION OR LIMITATION OF LIABILITY FOR CONSEQUENTIAL OR
+   INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU.
+#>
+
+# Pop out disclaimer
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+[Windows.Forms.MessageBox]::Show("
+THIS CODE IS SAMPLE CODE. 
+
+THESE SAMPLES ARE PROVIDED 'AS IS' WITHOUT WARRANTY OF ANY KIND.
+
+MICROSOFT FURTHER DISCLAIMS ALL IMPLIED WARRANTIES INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR OF FITNESS FOR A PARTICULAR PURPOSE. 
+
+THE ENTIRE RISK ARISING OUT OF THE USE OR PERFORMANCE OF THE SAMPLES REMAINS WITH YOU.
+
+IN NO EVENT SHALL MICROSOFT OR ITS SUPPLIERS BE LIABLE FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING OUT OF THE USE OF OR INABILITY TO USE THE SAMPLES, EVEN IF MICROSOFT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+BECAUSE SOME STATES DO NOT ALLOW THE EXCLUSION OR LIMITATION OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU.", "***DISCLAIMER***", [Windows.Forms.MessageBoxButtons]::OK, [Windows.Forms.MessageBoxIcon]::Warning)
+
+Write-Host "Checking if MicrosoftTeams module exists..."
+If (-not(Get-InstalledModule MicrosoftTeams -ErrorAction silentlycontinue)) {
+    Write-Host "404: MicrosoftTeams module not found. Please install Teams PowerShell module." -ForegroundColor DarkRed
+  }
+  Else {
+    Write-Host "MicrosoftTeams module exists... Continue we shall... " -ForegroundColor Green
+  }
+
+# Import Teams Module
+Import-Module MicrosoftTeams
+
+# Connects to Microsoft Teams
+Write-Host "Connecting to the Mother Ship (Microsoft Teams)..."
+Connect-MicrosoftTeams
+
+$FromDialPlan = Get-CsTenantDialPlan | Select-Object Identity, Description | Out-GridView -OutputMode Single -Title "Please select a Dial Plan to copy from."
+(Get-CsTenantDialPlan -Identity $FromDialPlan.Identity).NormalizationRules
+Write-Host "Please check dial plan normalisation rules above. If happy to proceed, press any key to continue"
+Pause
+$ToDialPlan = Get-CsTenantDialPlan | Select-Object Identity, Description | Out-GridView -OutputMode Single -Title "Please select a Dial Plan to copy to."
+$NR=(Get-CsTenantDialPlan -Identity $FromDialPlan.Identity).NormalizationRules
+Set-CsTenantDialPlan -Identity $ToDialPlan.Identity -NormalizationRules @{add=$nr}
+Write-Host "Normalisation Rules copied from $($FromDialPlan.Identity) to $($ToDialPlan.Identity)"
+(Get-CsTenantDialPlan -Identity $ToDialPlan.Identity).NormalizationRules
+Write-Host "Script completed"
---
+++ README.md
@@ -0,0 +1,32 @@
+# Copy Teams Dial Plan Normalisation Rules to another Dial Plan
+
+## Background
+This script will copy Teams Dial Plan Normalisation Rules to another Dial Plan.
+
+## Documentation
+Oooops... I've not completed this yet. My bad. 
+
+## Support
+If you encounter any bugs or issues while using the scripts, please raise them as issues [here](https://github.com/robwatts365/MicrosoftTeamsPhone-CopyDialPlan/issues) in this GitHub repository. You can also submit pull requests with your suggestions or improvements to the code. The project team appreciates your feedback and support. 
+
+Thank you for using this project and I hope you find it useful and helpful. 😊
+
+## Pre-requisites
+* PowerShell modules:
+  * MicrosoftTeams
+
+## Deployment Guide
+Oooops... I've not completed this yet. My bad. 
+If you think something is missing or incorrect, please raise them as issues [here](https://github.com/robwatts365/MicrosoftTeamsPhone-CopyDialPlan/issues).
+
+## Disclaimer
+> [!IMPORTANT]
+> _These samples are provided "as is" without warranty of any kind. Microsoft further disclaims all implied warranties including without limitation any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the samples remains with you. In no event shall Microsoft or its suppliers be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the samples, even if Microsoft has been advised of the possibility of such damages. Because some states do not allow the exclusion or limitation of liability for consequential or incidental damages, the above limitation may not apply to you._
+
+## Page info
+
+| Name | README.md |
+| :--- | :--- |
+| Author | Rob Watts ([@robwatts365](https://github.com/robwatts365)) |
+| **Version** | 1.0 |
+| **Date** | 14/01/2025 |
